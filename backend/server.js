ab2be4bda036d3c687e3565836d4fe607da75e1c
Amulya Reddy <amulyareddycheguri@gmail.com>
2026-05-18 22:20:55 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3001"] }));
+app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3001", "https://pms-e2ex.onrender.com"] }));
 app.use(express.json());
 app.use(bodyParser.json());
 
